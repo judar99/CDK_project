34db2c1f301f4c46fe6f50301fa87e4d79982d9a
judar99 <judaar.99@gmail.com>
2023-04-13 21:49:31 -0500

    update

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -138,24 +138,6 @@ export class CdkStack extends cdk.Stack {
       }
     });
 
-    const distributionConfig = inventoryFront.node.defaultChild as cloudfront.CfnDistribution;
-    distributionConfig.addPropertyOverride('DistributionConfig.DefaultCacheBehavior.ForwardedValues', {
-      QueryString: true,
-      Cookies: {
-        Forward: 'all',
-      },
-      Headers: [
-        'Authorization',
-      ],
-    });
-
-    distributionConfig.addPropertyOverride('DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations', [
-      {
-        EventType: 'viewer-request',
-        LambdaFunctionARN: 'arn:aws:lambda:us-east-1:123456789012:function:MyViewerRequestFunction',
-      },
-    ]);
-
     const userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClient', {
       userPool: userPool,
       authFlows: { 
@@ -172,7 +154,7 @@ export class CdkStack extends cdk.Stack {
           implicitCodeGrant: true
         },
         callbackUrls: [
-          `https://${inventoryFront.distributionDomainName}`
+          `${inventoryFront.distributionDomainName}/oauth2/idpresponse`
         ],
         logoutUrls: [
           'https://my-cloudfront-distribution.cloudfront.net/logout'
